f0fdb5e925a294fc7383a24e2c04cd052eefa9eb
LEANDRO RAVELO <LRC980728@GMAIL.COM>
2024-08-14 14:44:01 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -25,12 +25,11 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/* Elements */
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseButton = document.querySelector("#profile-close-button");
-
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
@@ -38,10 +37,37 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/* Functions */
 
 function closePopop() {
   profileEditModal.classList.remove("modal__opened");
-};
+}
+
+function getCardElement(cardData) {
+const cardElement = cardTemplate.cloneNode(true);
+const cardImageEl = cardElement.querySelector(".card__image");
+const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  return cardElement;
+}
+
+/* Event Handlers */
+
+function handlerProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopop();
+}
+
+/* Event Listeners */
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -50,13 +76,11 @@ profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal__opened");
 });
 
-profileCloseButton.addEventListener("click", () => {
-  closePopop();
-});
+profileCloseButton.addEventListener("click", closePopop);
 
-profileEditForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  profileTitle.textContent = profileTitleInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
-  closePopop();
+profileEditForm.addEventListener("submit", handlerProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
